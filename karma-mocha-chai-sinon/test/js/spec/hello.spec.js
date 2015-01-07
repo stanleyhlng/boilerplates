6a967b5102bf0dee6eef5ea30fcc1819412793f8
stanleyn <stanleyhlng@yahoo.com.hk>
2015-01-07 15:59:14 -0800

    add timing spec

---
+++ karma-mocha-chai-sinon/test/js/spec/hello.spec.js
@@ -0,0 +1,24 @@
+window.hello = function () {
+    return "Hello World";
+};
+
+describe("Trying out the test libaries", function () {
+
+    describe("Chai", function () {
+        it("should be equal using 'expect'", function () {
+            expect(hello()).to.equal("Hello World");
+        });
+    });
+
+    describe("Sinon.JS", function () {
+        it("should report spy called", function () {
+            var helloSpy = sinon.spy(window, 'hello');
+
+            expect(helloSpy.called).to.be.false;
+            hello();
+            expect(helloSpy.called).to.be.true;
+            hello.restore();
+        });
+    });
+
+});
