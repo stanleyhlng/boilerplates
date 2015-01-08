ba5ef3e006997ca8c7bbc979e3e10b9005d3a7d7
stanleyn <stanleyhlng@yahoo.com.hk>
2015-01-07 17:10:14 -0800

    update karma.conf.js

--- karma-mocha-chai-sinon/karma.conf.js
+++ karma-mocha-chai-sinon/karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'app/js/**/*.js': ['coverage']
     },
 
 
@@ -56,7 +57,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
