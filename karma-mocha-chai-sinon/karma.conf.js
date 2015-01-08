634528495fc32c6814b72c6354abc5bb058cc4d4
stanleyn <stanleyhlng@yahoo.com.hk>
2015-01-07 16:36:19 -0800

    updated karma conf

--- karma-mocha-chai-sinon/karma.conf.js
+++ karma-mocha-chai-sinon/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: 'src',
+    basePath: '',
 
 
     // frameworks to use
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'js/**/*.js',
-        'test/**/*.spec.js'
+        //'js/**/*.js',
+        'test/js/**/*.spec.js'
     ],
 
 
