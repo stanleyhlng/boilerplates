785829dc3bf413673d3c9ecb8fb645817bba6df6
stanleyn <stanleyhlng@yahoo.com.hk>
2015-01-08 18:02:21 -0800

    add namespace.spec

--- karma-mocha-chai-sinon/karma.conf.js
+++ karma-mocha-chai-sinon/karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        //'js/**/*.js',
+        'app/js/app/namespace.js',
         'test/js/**/*.spec.js'
     ],
 
