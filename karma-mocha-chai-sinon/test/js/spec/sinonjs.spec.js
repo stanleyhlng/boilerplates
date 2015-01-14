89177a0276c6a995cdc2606cabbcd8842c4cddda
stanleyn <stanleyhlng@yahoo.com.hk>
2015-01-14 14:25:13 -0800

    - add -w117 in .jshintrc - add test case for anonymous spies

--- karma-mocha-chai-sinon/test/js/spec/sinonjs.spec.js
+++ karma-mocha-chai-sinon/test/js/spec/sinonjs.spec.js
@@ -4,6 +4,24 @@ describe("Sinon.JS", function () {
 
         it("calls anonymous spy on event", function () {
             // Anonymous spies
+            var eventer = _.extend({}, Backbone.Events),
+                spy = sinon.spy();
+
+            // Set up the spy.
+            eventer.on("foo", spy);
+            expect(spy.called).to.be.false;
+
+            // Fire events.
+            eventer.trigger("foo", 42);
+
+            // Check number of calls.
+            expect(spy.calledOnce).to.be.true;
+            expect(spy.callCount).to.equal(1);
+
+            // Check calling argument
+            expect(spy.firstCall.args[0]).to.equal(42);
+            expect(spy.calledWith(42)).to.be.true;
+
         });
 
     });
